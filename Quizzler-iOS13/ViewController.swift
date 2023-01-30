c3af9290adb899c1e422cd6ba23139740e8c41e6
DmitryShcherba <debaucep@yandex.ru>
2023-01-30 20:02:23 +0300

    Bug fix. - Now you can't go out of Array range. Loop questions. - Now when you reach the final question, test starts from the very beginning.

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -16,32 +16,55 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     let quiz = [
-        "Question 1",
-        "Question 2",
-        "Question 3"
+        ["Moscow is Russia", "True"],
+        ["Oregon is Canada", "False"],
+        ["China is the biggest country on territory basis", "False"]
     ]
     
     var questionNumber = 0
     
+    
+    func updateUI () {
+        questionLabel.text = quiz[questionNumber][0] // question label
+    }
+        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         updateUI()
     
     }
 
+    
+    
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        questionNumber += 1
-        print (questionNumber)
-        updateUI()
-            
-        }
-    
-    func updateUI () {
-        questionLabel.text = quiz[questionNumber]
-    }
         
-    
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1] // answer from 2d Array
+        
+        
+        if userAnswer == actualAnswer {
+            print ("Right")
+        } else {
+            print ("Wrong")
+        }
+        
+        // verification of the lenght of the Array with quiz data
+        
+        
+        print ("Question Nr is ... \(questionNumber)")
+        
+        if questionNumber < quiz.count - 1 {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        
+        //print ("Question Nr is ... \(questionNumber)")
+        
+        updateUI()
+        
+        }
     
     }
     
